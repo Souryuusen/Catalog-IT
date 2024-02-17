6a1ef054d4dd12fb17ad56cb40674108cfd20640
GL <g.l@gmail.com>
2024-02-17 23:59:53 +0100

    Spring Initial Config

---
+++ backend/src/main/java/com/soursoft/catalogit/CatalogitApplication.java
@@ -0,0 +1,13 @@
+package com.soursoft.catalogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soursoft/catalogit/CatalogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soursoft.catalogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
