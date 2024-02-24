c201d361c976d0c8238e52e13e79289a41e0e64b
GL <g.l@gmail.com>
2024-02-24 16:01:56 +0100

    Test Angular Application

--- frontend/catalog-it/src/app/app.component.ts
+++ frontend/catalog-it/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent, RouterModule],
+  template:
+  `
+    <main>
+        <header class="brand-name">
+          <img class="brand-logo" src="./assets/logo.svg" alt="Logo" aria-hidden="true">
+        </header>
+        <section class="content">
+        <router-outlet></router-outlet>
+        </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'catalog-it';
+  title = 'catalog-it'
 }
