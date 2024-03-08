e9b9a41a613780a59eadbc6bde58ef14f9f0d541
Souryuusen <16085670+souryuusen@users.noreply.github.com>
2024-03-08 23:28:48 +0100

    Add: application.properties updated with jackson configuration Add: AuthToken generation based on username and encrypted password Add: Security configuration for using Basic Auth

--- backend/src/main/java/com/soursoft/catalogit/config/SecurityConfig.java
+++ backend/src/main/java/com/soursoft/catalogit/config/SecurityConfig.java
@@ -4,9 +4,15 @@ import com.soursoft.catalogit.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,6 +26,19 @@ public class SecurityConfig {
         auth.setUserDetailsService(userService);
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> {
+                  csrf.disable();
+                })
+                .authorizeHttpRequests(authorize -> {
+                  authorize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 
--- backend/src/main/java/com/soursoft/catalogit/controller/AuthController.java
+++ backend/src/main/java/com/soursoft/catalogit/controller/AuthController.java
@@ -4,12 +4,15 @@ import com.soursoft.catalogit.dto.UserDTO;
 import com.soursoft.catalogit.exception.UserAlreadyExistException;
 import com.soursoft.catalogit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@RestController
 @RequestMapping("api/auth")
 public class AuthController {
 
@@ -30,8 +33,12 @@ public class AuthController {
         if(userService.userExist(user)) {
             throw new UserAlreadyExistException("User with provided credentials already exist!");
         }
-        var registerdUser = userService.registerUser(user);
-        return UserDTO.fromUser(registerdUser);
+        var registeredUser = userService.registerUser(user);
+        String toEncode = registeredUser.getUsername() + ":" + registeredUser.getPassword().substring(registeredUser.getPassword().length()-31, registeredUser.getPassword().length());
+        var tempAuthToken = Base64.getEncoder().encodeToString(toEncode.getBytes(StandardCharsets.UTF_8));
+        var registeredDTO = UserDTO.fromUser(registeredUser);
+        registeredDTO.setAuthToken(tempAuthToken);
+        return registeredDTO;
     }
 
 }
