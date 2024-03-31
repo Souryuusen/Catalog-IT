e2fd5418354246b6cd477fa17c7c8e46e5c25f13
Souryuusen <16085670+souryuusen@users.noreply.github.com>
2024-03-31 22:53:42 +0200

    Add: Angular Material to the project

--- frontend/catalog-it/src/main.ts
+++ frontend/catalog-it/src/main.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes'
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import AuthenticationInterceptor from './app/auth/Interceptors/authenticationInterceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routeConfig),
     {
       provide:  HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true
-    }
+    }, provideAnimationsAsync()
   ]
 }).catch((err) => console.error(err));
